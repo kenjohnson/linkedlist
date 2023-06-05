aa5b6febec13e11666da871b37aafd39dbb7a78f
unknown <kenjtel@gmail.com>
2023-06-05 15:48:12 -0700

    put build list and print list outside\ changed to remove current instead of trailing duplicate

--- RemoveDuplicatesfromSortedList.py
+++ RemoveDuplicatesfromSortedList.py
@@ -4,78 +4,6 @@ class ListNode(object):
         self.val = val
         self.next = next
 
-# Definition of list
-class SLinkedList:
-    def __init__(self):
-        self.head =None
-
-    # list all the nodes
-    def listPrint(self):
-        printval = self.head
-        print("print list")
-        while printval is not None:
-            print(printval.val)
-            printval = printval.next
-
-    # find node
-    def findNode(self, val ):
-        findval = self.head
-        while findval is not None:
-            if findval.val == val:
-                return findval
-            findval = findval.next
-        return None
-
-    # add node at the beginning
-    def addAtBeginning(self, newdata):
-        NewNode = ListNode(newdata)
-        NewNode.next = self.head
-        self.head = NewNode
-
-    # add node at the end
-    def addAtEnd(self,newdata):
-        NewNode = ListNode(newdata)
-        if self.head is None:
-            self.head = NewNode
-            return
-        laste = self.head
-        while laste.next:
-            laste = laste.next
-        laste.next = NewNode
-
-    # add node in between
-    def addInBetween(self, middle_node, newdata):
-        if middle_node is None:
-            print("node not in list")
-            return
-        NewNode = ListNode(newdata)
-        NewNode.next = middle_node.next
-        middle_node.next = NewNode
-
-    # remove a node
-    def removeNode( self, RemoveKey):
-        HeadVal = self.head
-
-        if HeadVal is not None:
-            if HeadVal.val == RemoveKey:
-                self.head = HeadVal.next
-                HeadVal = None
-                return
-            while HeadVal is not None:
-                if HeadVal.val == RemoveKey:
-                    break
-                prev = HeadVal
-                HeadVal = HeadVal.next
-
-            if HeadVal is None:
-                return
-
-            print (prev.val, HeadVal.next.val)
-            prev.next = HeadVal.next
-            print (prev.next.val)
-            HeadVal = None
-
-
 class Solution(object):
     def deleteDuplicates(self, head):
         """
@@ -83,52 +11,47 @@ class Solution(object):
         :rtype: ListNode
         """
 
-        # convert input list to linked list
+        if not head:
+            return
 
-        list1 = SLinkedList()
+        curr = head
+ 
+        while curr:
 
-        for value in head:
-            NewNode = ListNode(value)
-            if list1.head is None:
-                list1.head = NewNode
-            else:
-                prev.next = NewNode
+            while curr.next and curr.val == curr.next.val:
+                curr.next = curr.next.next
+        
+            curr = curr.next
+     
+        return head
+   
+# build a linked list from a list 
+def buildLinkedList(input):
 
-            prev = NewNode
+    if not input:
+        return
 
-        # print the new list
-        list1.listPrint()
+    for i in range(0,len(input)): 
+        if i == 0:
+            list = curr = ListNode(input[0])
+        else:
+            curr.next = ListNode(input[i])  
+            curr = curr.next
+    
+    return list
 
-        #delete duplicates
+# print list given a linked list
+def printList(list):
 
-        curr = list1.head
-        prev = list1.head
+    if not list:
+        print ([])
+        return
 
-        while curr is not None and curr.next:
+    printval = list
 
-            if curr.val == curr.next.val:
-
-                if list1.head == curr:
-                    list1.head = curr.next
-                prev.next = curr.next
-                curr = curr.next
-            else:
-                prev = curr
-                curr = curr.next
-
-
-        print ("done removing duplicates")
-
-        list1.listPrint()
-
-
-        #convert linked list back to list and return
-        res = []
-        listval = list1.head
-        while listval is not None:
-            res.append(listval.val)
-            listval = listval.next
-        return res
+    while printval is not None:
+        print(printval.val)
+        printval = printval.next
 
 def main():
     """
@@ -136,25 +59,50 @@ def main():
     :
     """
 
-    head = [ 1,1,2 ]
-    res = Solution().deleteDuplicates( head )
-    print (res)
+    # case for duplicates at the beginning of list
+    head = [1,1,2]
+    list = buildLinkedList(head)   
+    res = Solution().deleteDuplicates( list )
+    print("before calling printList")
+    printList (res)
+    print("after calling printList")
 
+    # case for duplicates at both beginning and end of list 
     head = [ 1,1,2, 3,3]
-    res = Solution().deleteDuplicates( head )
-    print (res)
+    list = buildLinkedList( head )
+    res = Solution().deleteDuplicates( list )
+    print("before calling printList")
+    printList (res)
+    print("after calling printList")
 
-    head = [1 ]
-    res = Solution().deleteDuplicates( head )
-    print (res)
+    # case for list of 1 
+    head = [1]
+    list = buildLinkedList( head )
+    res = Solution().deleteDuplicates( list )
+    print("before calling printList")
+    printList (res)
+    print("after calling printList")
 
+    # case for empty set 
     head = [ ]
+    list = buildLinkedList( head )
     res = Solution().deleteDuplicates( head )
-    print (res)
+    print("before calling printList")
+    printList (res)
 
-    head = [1,1,2,2,3,4,5,5,6,7]
-    res = Solution().deleteDuplicates( head )
-    print (res)
+    # case for no repeating duplicates
+    head = [1,2,3,4]
+    list = buildLinkedList( head )
+    res = Solution().deleteDuplicates( list )
+    print("before calling printList")
+    printList (res)
+
+    # case for repeating duplicates
+    head = [1,1,2,2,2,2,3,4,5,5,6,6,7]
+    list = buildLinkedList( head )
+    res = Solution().deleteDuplicates( list )
+    print("before calling printList")
+    printList (res)
 
 if __name__ == '__main__':
     main()
